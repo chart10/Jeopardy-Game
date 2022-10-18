9fa1c98deb02212a207323996eae835367b82dac
Christian Hart <christiantaylorhart@gmail.com>
2022-10-17 22:16:54 -0400

    created questions-answers.txt for data references

--- signup.php
+++ signup.php
@@ -15,6 +15,17 @@ TODO: Create HTML form for signup
         <title>Jeopardy Sign-up</title>
     </head>
     <body>
-
+    <?php
+        setcookie("visited", true);
+        if (isset($_COOKIE["visited"])): ?>
+        <h1>Welcome Back!</h1>
+    <?php else: ?>
+        <h1>Please enter your info to sign up...</h1>
+        <form action="signup-submit.php" method="post">
+            <label for="message">Enter some text:</label>
+                <input type="text" name="message">
+            </label>
+        </form>
+    <?php endif ?>
     </body>
 </html>
