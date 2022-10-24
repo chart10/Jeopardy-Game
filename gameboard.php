0a0320b9e4a6da0bad506216c3c76f5dcb968e2f
Christian Hart <christiantaylorhart@gmail.com>
2022-10-23 23:32:37 -0400

    simplified checking if user finished the game on gameboard.php

--- gameboard.php
+++ gameboard.php
@@ -1,76 +1,35 @@
 <!-- Christian Hart, Jamie Kouttu, Alex Diaz -->
 
 <!-- This is where we should include the code for the main page of an active game.
-TODO: HTML table of all questions in a Jeopardy grid layout.
-TODO: Design PHP logic that links each dollar value to a question_answer page
-TODO: Page should replace dollar value with "right"/"wrong" based on user answer
-TODO: User div that displays username and score at the bottom of the page (logout option too)
+TODO: HTML table of all questions in a Jeopardy grid layout. DONE!
+TODO: Design PHP logic that links each dollar value to a question_answer page DONE!
+TODO: Page should replace dollar value with "right"/"wrong" based on user answer DONE!
+TODO: User div that displays username and score at the bottom of the page (logout option too) DONE!
 TODO: Button that resets the game for the user?
 TODO: (OPTIONAL) Randomize a box as "Daily Double" that allows the user to wager money
 TODO: Link to leaderboard
 -->
 
 <?php
-session_start();
+    session_start();
+    // Initialize the session variables indicating whether each question has been answered at 0 at the start of the game
+    for ($i = 1; $i <= 25; $i++){
+        $question = "q" . $i;
+        if(!isset($_SESSION[$question]))
+            $_SESSION[$question] = 0;
+    }
 
-//initialize the session variables indicating whether each question has been answered at 0 at the start of the game
-if(!isset($_SESSION['q1']))
-    $_SESSION['q1'] = 0;
-if(!isset($_SESSION['q2']))
-    $_SESSION['q2'] = 0;
-if(!isset($_SESSION['q3']))
-    $_SESSION['q3'] = 0;
-if(!isset($_SESSION['q4']))
-    $_SESSION['q4'] = 0;
-if(!isset($_SESSION['q5']))
-    $_SESSION['q5'] = 0;
-if(!isset($_SESSION['q6']))
-    $_SESSION['q6'] = 0;
-if(!isset($_SESSION['q7']))
-    $_SESSION['q7'] = 0;
-if(!isset($_SESSION['q8']))
-    $_SESSION['q8'] = 0;
-if(!isset($_SESSION['q9']))
-    $_SESSION['q9'] = 0;
-if(!isset($_SESSION['q10']))
-    $_SESSION['q10'] = 0;
-if(!isset($_SESSION['q11']))
-    $_SESSION['q11'] = 0;
-if(!isset($_SESSION['q12']))
-    $_SESSION['q12'] = 0;
-if(!isset($_SESSION['q13']))
-    $_SESSION['q13'] = 0;
-if(!isset($_SESSION['q14']))
-    $_SESSION['q14'] = 0;
-if(!isset($_SESSION['q15']))
-    $_SESSION['q15'] = 0;
-if(!isset($_SESSION['q16']))
-    $_SESSION['q16'] = 0;
-if(!isset($_SESSION['q17']))
-    $_SESSION['q17'] = 0;
-if(!isset($_SESSION['q18']))
-    $_SESSION['q18'] = 0;
-if(!isset($_SESSION['q19']))
-    $_SESSION['q19'] = 0;
-if(!isset($_SESSION['q20']))
-    $_SESSION['q20'] = 0;
-if(!isset($_SESSION['q21']))
-    $_SESSION['q21'] = 0;
-if(!isset($_SESSION['q22']))
-    $_SESSION['q22'] = 0;
-if(!isset($_SESSION['q23']))
-    $_SESSION['q23'] = 0;
-if(!isset($_SESSION['q24']))
-    $_SESSION['q24'] = 0;
-if(!isset($_SESSION['q25']))
-    $_SESSION['q25'] = 0;
-
-$wrong=false;
-$correct=false;
-
-// Need a function to run for each question which decides whether to display "right" or "wrong" if
-// the question has been answered
-
+    // Need a function to run for each question which decides whether to display "right" or "wrong" if
+    // the question has been answered
+    function rightWrong($qnum, $num, $value) {
+        if($_SESSION[$qnum] == 0){ ?>
+            <button class="jepButton" type="submit" name="qnum" value="<?php print($num) ?>">$<?php print($value) ?></button></td>
+        <?php } else if($_SESSION[$qnum] == 1){ ?>
+            <button class="correct" type="submit" name="<?php print($qnum) ?>" value="<?php print($num) ?>" disabled>RIGHT!</button></td>
+        <?php } else if($_SESSION[$qnum] == 2){ ?>
+            <button class="wrong" type="submit" name="<?php print($qnum) ?>" value="<?php print($num) ?>" disabled>WRONG!</button></td>
+            <?php }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
@@ -82,62 +41,58 @@ $correct=false;
         <title>Jeopardy Game Board</title>
     </head>
     <body class="jepBoard">
-    <?php
-    //if session username not set, direct user to log in
-    if(!isset($_SESSION['username'])){
-        ?>
-        <div class="block-text">
-            <h2>You are not logged in. <a href="login.php">Log in</a> or <a href="registration.php">register</a> to play!</h2>
-        </div>
-    <?php
-    }
-    else{
-        if($_SESSION['q1']>0 && $_SESSION['q2']>0 && $_SESSION['q3']>0 && $_SESSION['q4']>0 && $_SESSION['q5']>0
-            && $_SESSION['q6']>0 && $_SESSION['q7']>0 && $_SESSION['q8']>0 && $_SESSION['q9']>0 && $_SESSION['q10']>0
-            && $_SESSION['q11']>0 && $_SESSION['q12']>0 && $_SESSION['q13']>0 && $_SESSION['q14']>0 && $_SESSION['q15']>0
-            && $_SESSION['q16']>0 && $_SESSION['q17']>0 && $_SESSION['q18']>0 && $_SESSION['q19']>0 && $_SESSION['q20']>0
-            && $_SESSION['q21']>0 && $_SESSION['q22']>0 && $_SESSION['q23']>0 && $_SESSION['q24']>0 && $_SESSION['q25']>0) {
-            //if all questions have been answered, show final score and link to leaderboard
-            //and store the user's score in the database if it is better than their previous top score
-
-            //connect to database
-            $database = mysqli_connect('localhost', 'root', 'root', 'users');
-            //if there is an error connecting to the database, stop the script and display the error
-            if (mysqli_connect_errno()) {
-            // If there is an error with the connection, stop the script and display the error.
-                exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-            }
-
-            $username = $_SESSION['username'];
-            $query = "SELECT * FROM users WHERE username='$username'";
-            //query to find the row containing the username
-            $result = mysqli_query($database, $query);
-            $user = mysqli_fetch_assoc($result);
-
-            if($user['topscore'] == null || $user['topscore'] < $_SESSION['score']){
-                //if user has no top score, or their previous top score is less than their current score,
-                //update the top score in the database
-                $score = $_SESSION['score'];
-                $query = "UPDATE users SET topscore = '$score' WHERE username ='$username'";
-                mysqli_query($database, $query);
-                echo "<h1>New personal record!</h1>";
-            }
-
-            ?>
+        <?php
+        // If session username not set, direct user to log in
+        if (!isset($_SESSION['username'])) {  ?>
             <div class="block-text">
-                <h2>Your final score is: $<?= $_SESSION['score']?>!</h2>
-                <a href="leaderboard.php">Click here to see the leaderboard!</a>
-                <p><a href="logout.php">Log out here!</a></p>
+                <h2>You are not logged in. <a href="login.php">Log in</a> or <a href="registration.php">register</a> to play!</h2>
             </div>
         <?php
-
         }
-        else{
-            //if session score is not set (i.e. the session has just started), sets the score at 0
-            if(!isset($_SESSION['score'])){
-                $_SESSION['score'] = 0;
+        else {      // Check if all questions have been answered
+            $gameboardFinished = true;
+            for ($i = 1; $i <= 25; $i++){
+                $question = "q" . $i;
+                if ($_SESSION[$question] == 0) {
+                    $gameboardFinished = false;
+                    break;
+                }
             }
-        ?>
+            //if all questions have been answered, show final score and link to leaderboard
+            //and store the user's score in the database if it is better than their previous top score
+            if ($gameboardFinished) {
+                //connect to database
+                $database = mysqli_connect('localhost', 'root', 'root', 'users');
+                //if there is an error connecting to the database, stop the script and display the error
+                if (mysqli_connect_errno()) {
+                    // If there is an error with the connection, stop the script and display the error.
+                    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+                }
+                $username = $_SESSION['username'];
+                $query = "SELECT * FROM users WHERE username='$username'";
+                $result = mysqli_query($database, $query); //query to find the row containing the username
+                $user = mysqli_fetch_assoc($result);
+
+                if ($user['topscore'] == null || $user['topscore'] < $_SESSION['score']) {
+                    //if user has no top score, or their previous top score is less than their current score,
+                    //update the top score in the database
+                    $score = $_SESSION['score'];
+                    $query = "UPDATE users SET topscore = '$score' WHERE username ='$username'";
+                    mysqli_query($database, $query); ?>
+                    <h1>New personal record!</h1>
+                <?php } ?>
+
+                <div class="block-text">
+                    <h2>Your final score is: $<?= $_SESSION['score']?>!</h2>
+                    <a href="leaderboard.php">Click here to see the leaderboard!</a>
+                    <p><a href="logout.php">Log out here!</a></p>
+                </div>
+            <?php } else {
+                //if session score is not set (i.e. the session has just started), sets the score at 0
+                if(!isset($_SESSION['score'])){
+                    $_SESSION['score'] = 0;
+                }
+            ?>
             <div class="user-heading">
                 <h2>PLAYER:&nbsp&nbsp<?= $_SESSION['username']?></h2>
                 <h2>SCORE:&nbsp&nbsp$<?= $_SESSION['score']?></h2>
@@ -204,15 +159,3 @@ $correct=false;
     </div>
     </body>
 </html>
-<?php
-    function rightWrong($qnum, $num, $value) {
-        if($_SESSION[$qnum] == 0){ ?>
-            <button class="jepButton" type="submit" name="qnum" value="<?php print($num) ?>">$<?php print($value) ?></button></td>
-        <?php } else if($_SESSION[$qnum] == 1){ ?>
-            <button class="correct" type="submit" name="<?php print($qnum) ?>" value="<?php print($num) ?>" disabled>RIGHT!</button></td>
-        <?php } else if($_SESSION[$qnum] == 2){ ?>
-            <button class="wrong" type="submit" name="<?php print($qnum) ?>" value="<?php print($num) ?>" disabled>WRONG!</button></td>
-            <?php } ?>
-
-    <?php }
-?>
