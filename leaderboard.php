a98aa85a9c36c70d8273254dc1e27037f129fae2
jakou <113464442+jakou@users.noreply.github.com>
2022-10-24 13:46:16 -0400

    Leaderboard with styling

--- leaderboard.php
+++ leaderboard.php
@@ -3,22 +3,34 @@
 <!-- This page should display a leaderboard of top scores of users
 -->
 <?php
-$database = mysqli_connect('localhost', 'root', 'root', 'users');
-//if there is an error connecting to the database, stop the script and display the error
-if (mysqli_connect_errno()) {
-// If there is an error with the connection, stop the script and display the error.
-exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 
-$query = "SELECT username, topscore FROM users order by topscore desc";
-$result = mysqli_query($database, $query);
+function leaderboard()
+{
+    $database = mysqli_connect('localhost', 'root', '', 'users');
+    //if there is an error connecting to the database, stop the script and display the error
+    if (mysqli_connect_errno()) {
+    // If there is an error with the connection, stop the script and display the error.
+        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+    }
 
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)){
-        printf("$%d - %s <br/>",
-            $row["topscore"],
-            $row["username"]
-        );
+    $query = "SELECT username, topscore FROM users order by topscore desc";
+    $result = mysqli_query($database, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        $rank = 1;
+        while ($row = mysqli_fetch_assoc($result)) {
+            printf("
+                <tr>
+                    <td>%d</td>
+                    <td>$%d</td>
+                    <td>%s</td>
+                </tr>",
+                $rank,
+                $row["topscore"],
+                $row["username"]
+            );
+            $rank = $rank + 1;
+        }
     }
 }
 
@@ -31,6 +43,16 @@ if (mysqli_num_rows($result) > 0) {
 </head>
 
 <body>
+<table class="leaderboard">
+    <tr>
+        <th><h1>Rank</h1></th>
+        <th><h1>Score</h1></th>
+        <th><h1>Username</h1></th>
+    </tr>
+    <?php
+        leaderboard();
+    ?>
+</table>
 <div class="footer">
     <ul>
         <li><a href="frontpage.php">Home</a></li>
